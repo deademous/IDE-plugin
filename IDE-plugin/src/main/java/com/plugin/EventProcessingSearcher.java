32e790cc6e5b104b4bd77d4e3a4b2528d9168b96
deademous <opolindmitry@gmail.com>
2026-04-10 01:50:26 +0300

    add caching

--- IDE-plugin/src/main/java/com/plugin/EventProcessingSearcher.java
+++ IDE-plugin/src/main/java/com/plugin/EventProcessingSearcher.java
@@ -5,18 +5,32 @@ import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.search.searches.ReferencesSearch;
+import com.intellij.psi.util.CachedValuesManager;
 import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.kotlin.psi.KtClassOrObject;
 import org.jetbrains.kotlin.psi.KtImportDirective;
-import org.jetbrains.kotlin.psi.KtTypeReference;
+import com.intellij.psi.util.CachedValueProvider;
+import com.intellij.psi.util.PsiModificationTracker;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class EventProcessingSearcher {
 
     public static List<PsiElement> findProcessing(@NotNull KtClassOrObject target, @NotNull GlobalSearchScope scope) {
+        Map<GlobalSearchScope, List<PsiElement>> scopeMap = CachedValuesManager.getCachedValue(target,
+                () -> {
+                    Map<GlobalSearchScope, List<PsiElement>> map = new ConcurrentHashMap<>();
+                    return CachedValueProvider.Result.create(map, PsiModificationTracker.MODIFICATION_COUNT);
+                });
+
+        return scopeMap.computeIfAbsent(scope, s -> search(target, s));
+    }
+
+    private static List<PsiElement> search(@NotNull KtClassOrObject target, @NotNull GlobalSearchScope scope) {
         List<PsiElement> results = new ArrayList<>();
 
         for (PsiReference ref : ReferencesSearch.search(target, scope, false).findAll()) {
